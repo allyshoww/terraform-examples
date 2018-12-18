df3f2ca9925f22bbfc9b58d23fb9e412f597bfa1
Allyson Oliveira <asoliveira@outlook.com>
2018-12-18 11:21:33 -0200

    Add S3 backend config

--- backend.tf
+++ backend.tf
@@ -1,24 +1,24 @@
 # Hashicorp Consul Backend
 
-#terraform {
-#backend "consul" {
-#address = “demo.consul.io" # hostname of consul cluster
-#path = “terraform/myproject”
-#}
-#}
+terraform {
+backend "consul" {
+address = “demo.consul.io" # hostname of consul cluster
+path = “terraform/myproject”
+}
+}
 
-# Stored on S3
-#terraform {
-#backend "s3" {
-#bucket = "mybucket"
-#key = “terraform/myproject”
-#region = “eu-west-1"
-#}
-#}
+ Stored on S3
+terraform {
+backend "s3" {
+bucket = "mybucket"
+key = “terraform/myproject”
+region = “eu-west-1"
+}
+}
 
-# Credentials using S3
-#aws configure
-#AWS Access Key ID []: AWS-key
-#AWS Secret Access Key []: AWS_secret_key
-#Default region name []: eu-west-1
-#Default output format [None]:
+ Credentials using S3
+aws configure
+AWS Access Key ID []: AWS-key
+AWS Secret Access Key []: AWS_secret_key
+Default region name []: eu-west-1
+Default output format [None]:
